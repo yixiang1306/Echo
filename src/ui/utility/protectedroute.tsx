83e4329d18d8f989c07f28036285e509b3c1e251
mario <khantkokozaw382000@gmail.com>
2025-02-09 18:51:53 +0800

    fix session

--- src/ui/utility/protectedroute.tsx
+++ src/ui/utility/protectedroute.tsx
@@ -1,27 +1,25 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useLoading } from "./loadingContext";
 import { useAuth } from "./authprovider";
 
 const ProtectedRoute = () => {
-  const { session } = useAuth();
-  const { isLoading } = useLoading();
+  const { session, isSessionLoading } = useAuth();
 
-  if (isLoading) {
+  if (isSessionLoading) {
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
         <div className="loader border-4 border-white border-t-transparent rounded-full w-12 h-12 animate-spin"></div>
         <p className="text-white mt-4">Authenticating...</p>
       </div>
     );
-  }
+  } else {
+    if (!session) {
+      console.log("No session found, redirecting to /");
+      return <Navigate to="/" replace />;
+    }
 
-  if (!session) {
-    console.log("🚫 No session found, redirecting to /");
-    return <Navigate to="/" replace />;
+    console.log("✅ Session found, rendering protected content");
+    return <Outlet />;
   }
-
-  console.log("✅ Session found, rendering protected content");
-  return <Outlet />;
 };
 
 export default ProtectedRoute;
